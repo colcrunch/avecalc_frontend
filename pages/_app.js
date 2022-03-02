89bc2d1648f4c073148551a6f1192fe1d18df574
colcrunch <rhartnett35@gmail.com>
2022-03-02 01:24:42 -0500

    Add chakra and write main calc.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import { ChakraProvider, extendTheme, useColorModeValue, ColorModeScript } from '@chakra-ui/react'
+
+const config = {
+  initialColorMode: 'dark',
+  ussSystemDefault: false,
+  colors: {}
+}
+
+
+
+const theme = extendTheme({ config })
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ChakraProvider theme={theme}>
+    <Component {...pageProps} />
+  </ChakraProvider>
+  )
 }
 
 export default MyApp
